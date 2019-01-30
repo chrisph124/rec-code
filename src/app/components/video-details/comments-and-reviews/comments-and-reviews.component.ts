16b328b9b22c35d836e0fc9acda87d4d127db6ff
vund-vn <haidt14@fsoft.com.vn>
2019-01-30 17:33:46 +0700

    edit layout

---
+++ src/app/components/video-details/comments-and-reviews/comments-and-reviews.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'comments-and-reviews',
+  templateUrl: './comments-and-reviews.component.html',
+  styleUrls: ['./comments-and-reviews.component.css']
+})
+export class CommentsAndReviewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/components/video-details/video-details.module.ts
+++ src/app/components/video-details/video-details.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { VideoDetailsComponent } from './video-details.component';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { VideoPlayerComponent } from './video-player/video-player.component';
+import { MovieInfoComponent } from './movie-info/movie-info.component';
+import { CommentsAndReviewsComponent } from './comments-and-reviews/comments-and-reviews.component';
 
 @NgModule({
-  declarations: [VideoDetailsComponent, VideoPlayerComponent],
+  declarations: [VideoDetailsComponent, VideoPlayerComponent, MovieInfoComponent, CommentsAndReviewsComponent],
   imports: [
     CommonModule,
     NgZorroAntdModule
